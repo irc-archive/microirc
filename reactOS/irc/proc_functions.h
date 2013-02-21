bf6e1255c56177471f3740b44796ab985ee6b0ff
diogoandre12@gmail.com <diogoandre12@gmail.com@86e1f2ac-c059-4b1f-8d1b-d1954550aa9b>
2013-02-21 17:42:36 +0000

    changes
    git-svn-id: svn://localhost/tmp/microirc/trunk@105 86e1f2ac-c059-4b1f-8d1b-d1954550aa9b

--- reactOS/irc/proc_functions.h
+++ reactOS/irc/proc_functions.h
@@ -34,9 +34,9 @@ INT_PTR CALLBACK AboutProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam){
    return FALSE;
 }
 
-INT_PTR CALLBACK PreferencesProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam){
-SetWindowLongPtr(hDlg, DWLP_MSGRESULT, FALSE);
-if(TRUE)return FALSE;
+INT_PTR CALLBACK PreferencesProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+//SetWindowLongPtr(hDlg, DWLP_MSGRESULT, FALSE);
+//if(TRUE)return FALSE;
 
     static wchar_t type[IRC_SIZE_SMALL];
     static wchar_t profile[IRC_SIZE_SMALL];
@@ -113,9 +113,9 @@ if(TRUE)return FALSE;
       case WM_NOTIFY:{
         LPPSHNOTIFY lppsn = (LPPSHNOTIFY) lParam;
         NMHDR nmhdr = lppsn->hdr;
-        if(nmhdr.code == PSN_KILLACTIVE){
-            MessageBox(NULL,L"killactive",NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-        }
+        //if(nmhdr.code == PSN_KILLACTIVE){
+        //    MessageBox(NULL,L"killactive",NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
+        //}
         if(nmhdr.code == PSN_APPLY){
             MessageBox(NULL,L"apply",NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
         }
@@ -182,256 +182,67 @@ if(TRUE)return FALSE;
    return FALSE;
 }
 
-BOOL CALLBACK mifuck(
-  _In_  HWND hwnd,
-  _In_  LPARAM lParam
-){
-    //MessageBox(NULL,L"GUD",NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-    return TRUE;
-}
-
-int CALLBACK TESTEPROC(HWND hDlgHwnd, UINT uMsg, LPARAM lParam){
-    HWND hDlg = PropSheet_GetTabControl(hDlgHwnd);
-    switch(uMsg){
-        case PSCB_PRECREATE:{
-            //DLGTEMPLATEEX dlgTemplate = lParam;
-            break;
-        }
-        case PSCB_INITIALIZED:{
-        EnumChildWindows(hDlg,mifuck, (LPARAM)NULL);
-             //wchar_t **parameters = (wchar_t**)lParam;//"MANAGER" or "CLIENT", profile
-             wchar_t *parameters[2]={L"MANAGER", L"Client.ini"};
-             iniparser_t iniparser;
-             if(iniparser_init(&iniparser)!=0){
-                break;
-             }
-             if(winiparser_load(&iniparser,parameters[1])!=0){
-                iniparser_destroy(&iniparser);
-                break;
-             }
-             settext_fromwstr(hDlg,IDC_EDIT0,parameters[0]);
-             settext_fromwstr(hDlg,IDC_EDIT1,parameters[1]);
-             settext_fromstr(hDlg,IDC_EDIT2,iniparser_getstring(&iniparser, IRC_CONF_SERVER, IRC_CONF_HOST, IRC_CONF_HOST_VAL));
-             settext_fromstr(hDlg,IDC_EDIT3,iniparser_getstring(&iniparser, IRC_CONF_SERVER, IRC_CONF_PORT, IRC_CONF_PORT_VAL));
-             settext_fromstr(hDlg,IDC_EDIT4,iniparser_getstring(&iniparser, IRC_CONF_SERVER, IRC_CONF_PASS, IRC_CONF_PASS_VAL));
-             settext_fromstr(hDlg,IDC_EDIT5,iniparser_getstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_USER, IRC_CONF_USER_VAL));
-             settext_fromstr(hDlg,IDC_EDIT6,iniparser_getstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_NAME, IRC_CONF_NAME_VAL));
-             settext_fromstr(hDlg,IDC_EDIT7,iniparser_getstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_NICK, IRC_CONF_NICK_VAL));
-             settext_fromstr(hDlg,IDC_EDIT8,iniparser_getstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_PERFORM, IRC_CONF_PERFORM_VAL));
-             settext_fromstr(hDlg,IDC_EDIT9,iniparser_getstring(&iniparser, IRC_CONF_AUTOJOIN, IRC_CONF_CHANNELS, IRC_CONF_CHANNELS_VAL));
-             settext_fromint(hDlg,IDC_EDIT10,iniparser_getint(&iniparser, IRC_CONF_AUTOJOIN, IRC_CONF_DELAY, IRC_CONF_DELAY_VAL));
-             if(wcscmp(parameters[0],IRC_CONST_MANAGER)==0){
-                setcheck_fromint(hDlg,IDC_CHECK1,iniparser_getint(&iniparser, IRC_CONF_CONNECTION, IRC_CONF_STARTUP, IRC_CONF_STARTUP_VAL));
-                settext_fromint(hDlg,IDC_EDIT11,iniparser_getint(&iniparser, IRC_CONF_CONNECTION, IRC_CONF_RETRIES, IRC_CONF_RETRIES_VAL));
-                settext_fromstr(hDlg,IDC_EDIT12,iniparser_getstring(&iniparser, IRC_CONF_MESSAGES, IRC_CONF_PART, IRC_CONF_PART_VAL));
-                settext_fromstr(hDlg,IDC_EDIT13,iniparser_getstring(&iniparser, IRC_CONF_MESSAGES, IRC_CONF_KICK, IRC_CONF_KICK_VAL));
-                settext_fromstr(hDlg,IDC_EDIT14,iniparser_getstring(&iniparser, IRC_CONF_MESSAGES, IRC_CONF_QUIT, IRC_CONF_QUIT_VAL));
-                setcombo_fromint(hDlg,IDC_COMBO1,iniparser_getint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_ENCODING, IRC_CONF_ENCODING_VAL));
-                settext_fromint(hDlg,IDC_EDIT15,iniparser_getint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_BUBBLE, IRC_CONF_BUBBLE_VAL));
-                setcheck_fromint(hDlg,IDC_CHECK2,iniparser_getint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_SOUNDS, IRC_CONF_SOUNDS_VAL));
-                settext_fromint(hDlg,IDC_EDIT16,iniparser_getint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_LEDNUMBER, IRC_CONF_LEDNUMBER_VAL));
-                settext_fromint(hDlg,IDC_EDIT17,iniparser_getint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_LEDINTERVAL, IRC_CONF_LEDINTERVAL_VAL));
-             }else if(wcscmp(parameters[0],IRC_CONST_CLIENT)==0){
-                setcheck_fromint(hDlg,IDC_CHECK1,client.config.connect_on_startup);
-                settext_fromint(hDlg,IDC_EDIT11,client.config.reconnect_retries);
-                settext_fromstr(hDlg,IDC_EDIT12,client.config.part);
-                settext_fromstr(hDlg,IDC_EDIT13,client.config.kick);
-                settext_fromstr(hDlg,IDC_EDIT14,client.config.quit);
-                if(client.config.encoding==CP_UTF8){
-                   setcombo_fromint(hDlg,IDC_COMBO1,1);
-                }else{
-                   setcombo_fromint(hDlg,IDC_COMBO1,0);
-                }
-                settext_fromint(hDlg,IDC_EDIT15,client.config.bubble);
-                setcheck_fromint(hDlg,IDC_CHECK2,client.config.sounds);
-                settext_fromint(hDlg,IDC_EDIT16,client.config.led_number);
-                settext_fromint(hDlg,IDC_EDIT17,client.config.led_interval);
-             }
-             iniparser_destroy(&iniparser);
-             return TRUE;
-            break;
-        }
-        case PSCB_BUTTONPRESSED:{
-             if(lParam == PSBTN_OK){
-                iniparser_t iniparser;
-                if(iniparser_init(&iniparser)!=0){
-                   break;
-                }
-                wchar_t type[IRC_SIZE_SMALL];
-                wchar_t profile[IRC_SIZE_SMALL];
-                char host[IRCPROTOCOL_SIZE_SMALL];
-                char port[IRCPROTOCOL_SIZE_SMALL];
-                char pass[IRCPROTOCOL_SIZE_SMALL];
-                char user[IRCPROTOCOL_SIZE_SMALL];
-                char name[IRCPROTOCOL_SIZE_SMALL];
-                char nick[IRCPROTOCOL_SIZE_SMALL];
-                char perform[IRCPROTOCOL_SIZE_MEDIUM];
-                char channels[IRCPROTOCOL_SIZE_MEDIUM];
-                int delay;
-                int connect_on_startup;
-                int reconnect_retries;
-                char part[IRC_SIZE_SMALL];
-                char kick[IRC_SIZE_SMALL];
-                char quit[IRC_SIZE_SMALL];
-                int encoding;
-                int bubble;
-                int sounds;
-                int led_number;
-                int led_interval;
-                gettext_towstr(hDlg,IDC_EDIT0,type,IRC_SIZE_SMALL);
-                gettext_towstr(hDlg,IDC_EDIT1,profile,IRC_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT2, host, IRCPROTOCOL_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT3, port, IRCPROTOCOL_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT4, pass, IRCPROTOCOL_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT5, user, IRCPROTOCOL_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT6, name, IRCPROTOCOL_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT7, nick, IRCPROTOCOL_SIZE_MEDIUM);
-                gettext_tostr(hDlg, IDC_EDIT8, perform, IRCPROTOCOL_SIZE_MEDIUM);
-                gettext_tostr(hDlg, IDC_EDIT9, channels, IRCPROTOCOL_SIZE_MEDIUM);
-                delay = gettext_toint(hDlg,IDC_EDIT10);
-                connect_on_startup = getint_fromcheck(hDlg,IDC_CHECK1);
-                reconnect_retries = gettext_toint(hDlg,IDC_EDIT11);
-                gettext_tostr(hDlg, IDC_EDIT12, part, IRC_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT13, kick, IRC_SIZE_SMALL);
-                gettext_tostr(hDlg, IDC_EDIT14, quit, IRC_SIZE_SMALL);
-                encoding = getint_fromcombo(hDlg,IDC_COMBO1);
-                bubble = gettext_toint(hDlg,IDC_EDIT15);
-                sounds = getint_fromcheck(hDlg,IDC_CHECK2);
-                led_number = gettext_toint(hDlg,IDC_EDIT16);
-                led_interval = gettext_toint(hDlg,IDC_EDIT17);
-                if(strlen(host)==0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG1),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(strlen(port)==0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG2),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(strlen(user)==0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG3),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(strlen(name)==0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG4),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(strlen(nick)==0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG5),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(delay < 0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG6),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(reconnect_retries < 0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG7),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(bubble < 0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG8),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                if(led_interval < 0){
-                   MessageBox(hDlg,MAKEINTSTR(IDS_ERROR_MSG9),NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
-                   break;
-                }
-                iniparser_setstring(&iniparser, IRC_CONF_SERVER, IRC_CONF_HOST, host);
-                iniparser_setstring(&iniparser, IRC_CONF_SERVER, IRC_CONF_PORT, port);
-                iniparser_setstring(&iniparser, IRC_CONF_SERVER, IRC_CONF_PASS, pass);
-                iniparser_setstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_USER, user);
-                iniparser_setstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_NAME, name);
-                iniparser_setstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_NICK, nick);
-                iniparser_setstring(&iniparser, IRC_CONF_CLIENT, IRC_CONF_PERFORM, perform);
-                iniparser_setstring(&iniparser, IRC_CONF_AUTOJOIN, IRC_CONF_CHANNELS, channels);
-                iniparser_setint(&iniparser, IRC_CONF_AUTOJOIN, IRC_CONF_DELAY, delay);
-                iniparser_setint(&iniparser, IRC_CONF_CONNECTION, IRC_CONF_STARTUP, connect_on_startup);
-                iniparser_setint(&iniparser, IRC_CONF_CONNECTION, IRC_CONF_RETRIES, reconnect_retries);
-                iniparser_setstring(&iniparser, IRC_CONF_MESSAGES, IRC_CONF_PART, part);
-                iniparser_setstring(&iniparser, IRC_CONF_MESSAGES, IRC_CONF_KICK, kick);
-                iniparser_setstring(&iniparser, IRC_CONF_MESSAGES, IRC_CONF_QUIT, quit);
-                iniparser_setint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_ENCODING, encoding);
-                iniparser_setint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_BUBBLE, bubble);
-                iniparser_setint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_SOUNDS, sounds);
-                iniparser_setint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_LEDNUMBER, led_number);
-                iniparser_setint(&iniparser, IRC_CONF_MISCELLANEOUS, IRC_CONF_LEDINTERVAL, led_interval);
-                if(wcscmp(type,IRC_CONST_CLIENT)==0){
-                   client.config.connect_on_startup = connect_on_startup;
-                   client.config.reconnect_retries = reconnect_retries;
-                   strncpy0(client.config.part,part,IRC_SIZE_SMALL);
-                   strncpy0(client.config.kick,kick,IRC_SIZE_SMALL);
-                   strncpy0(client.config.quit,quit,IRC_SIZE_SMALL);
-                   if(encoding!=0){
-                      client.config.encoding=CP_UTF8;
-                   }else{
-                      client.config.encoding=CP_ACP;
-                   }
-                   client.config.bubble=bubble;
-                   client.config.sounds=sounds;
-                   client.config.led_number=led_number;
-                   client.config.led_interval=led_interval;
-                }
-                if(winiparser_store(&iniparser,profile)!=0){
-                   iniparser_destroy(&iniparser);
-                   break;
-                }
-                iniparser_destroy(&iniparser);
-                return TRUE;
-             }else if (lParam == PSBTN_CANCEL || lParam == PSBTN_FINISH){
-                return FALSE;
-             }
-            break;
-        }
+INT_PTR WINAPI OpenPreferencesDialog(HWND hWndParent, LPARAM dwInitParam) {
+    wchar_t **parameters = (wchar_t**)dwInitParam;
+    iniparser_t iniparser;
+    if(iniparser_init(&iniparser)!=0){
+        return FALSE;
     }
-    return FALSE;
-}
+    if(winiparser_load(&iniparser,parameters[1])!=0){
+        iniparser_destroy(&iniparser);
+        return FALSE;
+    }
 
-INT_PTR WINAPI OpenPreferencesDialog(HWND hWndParent, LPARAM dwInitParam){
     PROPSHEETPAGE psp[4];
+
+    void *page0[3] = {&iniparser, L"0", parameters[0], parameters[1]};
+    psp[0].dwSize      = sizeof(PROPSHEETPAGE);
+    psp[0].dwFlags     = PSP_USEICONID | PSP_USETITLE; //PSP_USECALLBACK
+    psp[0].hInstance   = config.h_instance;
+    psp[0].pszTemplate = MAKEINTRESOURCE(IDT_TAB1);
+    psp[0].pszIcon     = NULL; //MAKEINTRESOURCE(IDI_FONT);
+    psp[0].pfnDlgProc  = PreferencesProc;
+    psp[0].pszTitle    = L"0";
+    psp[0].lParam      = (LPARAM)page0;
+    psp[0].pfnCallback = NULL;
+
+    wchar_t *page1[3] = {&iniparser, L"1", parameters[0], parameters[1]};
+    psp[1].dwSize      = sizeof(PROPSHEETPAGE);
+    psp[1].dwFlags     = PSP_USEICONID | PSP_USETITLE; //PSP_USECALLBACK
+    psp[1].hInstance   = config.h_instance;
+    psp[1].pszTemplate = MAKEINTRESOURCE(IDT_TAB2);
+    psp[1].pszIcon     = NULL; //MAKEINTRESOURCE(IDI_BORDER);
+    psp[1].pfnDlgProc  = PreferencesProc;
+    psp[1].pszTitle    = L"1";
+    psp[1].lParam      = (LPARAM)page1;
+    psp[1].pfnCallback = NULL;
+
+    wchar_t *page2[3] = {&iniparser, L"2", parameters[0], parameters[1]};
+    psp[2].dwSize      = sizeof(PROPSHEETPAGE);
+    psp[2].dwFlags     = PSP_USEICONID | PSP_USETITLE; //PSP_USECALLBACK
+    psp[2].hInstance   = config.h_instance;
+    psp[2].pszTemplate = MAKEINTRESOURCE(IDT_TAB3);
+    psp[2].pszIcon     = NULL; //MAKEINTRESOURCE(IDI_BORDER);
+    psp[2].pfnDlgProc  = PreferencesProc;
+    psp[2].pszTitle    = L"2";
+    psp[2].lParam      = (LPARAM)page2;
+    psp[2].pfnCallback = NULL;
+
+    wchar_t *page3[3] = {&iniparser, L"3", parameters[0], parameters[1]};
+    psp[3].dwSize      = sizeof(PROPSHEETPAGE);
+    psp[3].dwFlags     = PSP_USEICONID | PSP_USETITLE; //PSP_USECALLBACK
+    psp[3].hInstance   = config.h_instance;
+    psp[3].pszTemplate = MAKEINTRESOURCE(IDT_TAB4);
+    psp[3].pszIcon     = NULL; //MAKEINTRESOURCE(IDI_BORDER);
+    psp[3].pfnDlgProc  = PreferencesProc;
+    psp[3].pszTitle    = L"3";
+    psp[3].lParam      = (LPARAM)page3;
+    psp[3].pfnCallback = NULL; 
 
     PROPSHEETHEADER psh;
 
-    psp[0].dwSize      = sizeof(PROPSHEETPAGE);
-    psp[0].dwFlags     = PSP_USEICONID | PSP_USETITLE;
-    psp[0].hInstance   = config.h_instance;
-    psp[0].pszTemplate = MAKEINTRESOURCE(IDT_TAB1);
-    psp[0].pszIcon     = NULL;//MAKEINTRESOURCE(IDI_FONT);
-    psp[0].pfnDlgProc  = PreferencesProc;
-    psp[0].pszTitle    = L"0";
-    psp[0].lParam      = dwInitParam;
-    psp[0].pfnCallback = NULL;
-
-    psp[1].dwSize      = sizeof(PROPSHEETPAGE);
-    psp[1].dwFlags     = PSP_USEICONID | PSP_USETITLE;
-    psp[1].hInstance   = config.h_instance;
-    psp[1].pszTemplate = MAKEINTRESOURCE(IDT_TAB2);
-    psp[1].pszIcon     = NULL;//MAKEINTRESOURCE(IDI_BORDER);
-    psp[1].pfnDlgProc  = PreferencesProc;
-    psp[1].pszTitle    = L"1";
-    psp[1].lParam      = dwInitParam;
-    psp[1].pfnCallback = NULL;
-
-    psp[2].dwSize      = sizeof(PROPSHEETPAGE);
-    psp[2].dwFlags     = PSP_USEICONID | PSP_USETITLE;
-    psp[2].hInstance   = config.h_instance;
-    psp[2].pszTemplate = MAKEINTRESOURCE(IDT_TAB3);
-    psp[2].pszIcon     = NULL;//MAKEINTRESOURCE(IDI_BORDER);
-    psp[2].pfnDlgProc  = PreferencesProc;
-    psp[2].pszTitle    = L"2";
-    psp[2].lParam      = dwInitParam;
-    psp[2].pfnCallback = NULL;
-
-    psp[3].dwSize      = sizeof(PROPSHEETPAGE);
-    psp[3].dwFlags     = PSP_USEICONID | PSP_USETITLE;
-    psp[3].hInstance   = config.h_instance;
-    psp[3].pszTemplate = MAKEINTRESOURCE(IDT_TAB4);
-    psp[3].pszIcon     = NULL;//MAKEINTRESOURCE(IDI_BORDER);
-    psp[3].pfnDlgProc  = PreferencesProc;
-    psp[3].pszTitle    = L"3";
-    psp[3].lParam      = dwInitParam;
-    psp[3].pfnCallback = NULL; //PSP_USECALLBACK
-
     psh.dwSize      = sizeof(PROPSHEETHEADER);
-    psh.dwFlags     = PSH_USEICONID | PSH_PROPSHEETPAGE | PSH_NOAPPLYNOW | PSH_NOCONTEXTHELP | PSH_PROPTITLE | PSH_USECALLBACK;
+    psh.dwFlags     = PSH_USEICONID | PSH_PROPSHEETPAGE | PSH_NOAPPLYNOW | PSH_NOCONTEXTHELP | PSH_PROPTITLE; // | PSH_USECALLBACK;
     psh.hwndParent  = hWndParent;
     psh.hInstance   = config.h_instance;
     psh.pszIcon     = NULL;//MAKEINTRESOURCE(IDI_CELL_PROPERTIES);
@@ -439,9 +250,16 @@ INT_PTR WINAPI OpenPreferencesDialog(HWND hWndParent, LPARAM dwInitParam){
     psh.nPages      = sizeof(psp) / sizeof(PROPSHEETPAGE);
     psh.nStartPage  = 0;
     psh.ppsp        = (LPCPROPSHEETPAGE) &psp;
-    psh.pfnCallback = TESTEPROC;
+    //psh.pfnCallback = TESTEPROC;
 
     int result = PropertySheet(&psh);
+
+    if(winiparser_store(&iniparser,profile)!=0){
+        iniparser_destroy(&iniparser);
+        return FALSE;
+    }
+    iniparser_destroy(&iniparser);
+    
     if(result > 0){
         //MessageBox(NULL,L"GUD",NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
         return TRUE;
@@ -449,6 +267,7 @@ INT_PTR WINAPI OpenPreferencesDialog(HWND hWndParent, LPARAM dwInitParam){
         //MessageBox(NULL,L"BAD",NULL,MB_ICONHAND|MB_APPLMODAL|MB_SETFOREGROUND);
         return FALSE;
      }
+    
 /*MSG msg;
 BOOL bret;
 while ((bret = GetMessage (&msg, NULL, 0, 0)) != 0){
