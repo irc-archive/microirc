5a30e4b3d83bf5ed89e7c9ecc3ed066c36321544
diogoandre12@gmail.com <diogoandre12@gmail.com@86e1f2ac-c059-4b1f-8d1b-d1954550aa9b>
2013-08-07 20:34:39 +0000

    my projects
    git-svn-id: svn://localhost/tmp/microirc/trunk@132 86e1f2ac-c059-4b1f-8d1b-d1954550aa9b

---
+++ myproject/MyOSGi/src/example/Activator.java
@@ -0,0 +1,18 @@
+package example;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("i startedddddddddddddddddddddddddddd");
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("i stopppppppppppppppppppppppppppppppp");
+	}
+
+}
---
+++ myproject/MyOSGi/src/example/priv/MyPrivClass.java
@@ -0,0 +1,14 @@
+package example.priv;
+
+public class MyPrivClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void naoFacoNada() {
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa");
+	}
+
+}
